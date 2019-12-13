34e6b81094bdc943e8aa6c8b9519d287d1fd4e0d
Darrell Lamm <darrelllamm0@gmail.com>
2019-12-13 11:52:18 -0500

    New define to activate Anaren board (27MHz) initialization data
    Added a define to possibly activate alternative settings for Anaren (and potentially other) boards that operate at 27MHz instead of the typical 26MHz!

--- libraries/AirMiniTransmitter/config.h
+++ libraries/AirMiniTransmitter/config.h
@@ -3,3 +3,8 @@
 #undef TRANSMIT
 // For transmitter
 #define TRANSMIT
+
+// For Anaren transeiver boards that operate at 27MHz
+#define ANAREN
+// For other transeiver boards that operate at 26MHz
+#undef ANAREN
