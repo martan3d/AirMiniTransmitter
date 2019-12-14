0e3780c71afd62b4cbd5368129191a1419af2d97
Darrell Lamm <darrelllamm0@gmail.com>
2019-12-14 08:17:00 -0500

    Moved config.h to a better-named directory

---
+++ libraries/config/config.h
@@ -0,0 +1,10 @@
+// The LAST entry is active!
+// For receiver
+#undef TRANSMIT
+// For transmitter
+#define TRANSMIT
+
+// For Anaren transeiver boards that operate at 27MHz
+#define ANAREN
+// For other transeiver boards that operate at 26MHz
+#undef ANAREN
