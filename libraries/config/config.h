56ec5f0e2caf5034f269a733bd99a5cd02123ce2
Darrell Lamm <darrelllamm0@gmail.com>
2020-01-29 17:12:02 -0500

    Corrected spelling in comments

--- libraries/config/config.h
+++ libraries/config/config.h
@@ -4,11 +4,13 @@
 // For receiver
 #undef TRANSMIT
 
-// For Anaren transeiver boards that operate at 27MHz
+// The LAST entry is active!
+// For Anaren transceiver boards that operate at 27MHz
 #define ANAREN
-// For other transeiver boards that operate at 26MHz
+// For other transceiver boards that operate at 26MHz
 #undef ANAREN
 
+// Determined from defines above
 // Define whether is or is NOT operating @27 MHz
 #ifdef ANAREN
 #define TWENTY_SEVEN_MHZ
